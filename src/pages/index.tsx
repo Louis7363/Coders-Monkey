8db596d851dc7101622ff4d0af558c84a8d5a11f
Louis7363 <louiscrt@sfr.fr>
2024-12-30 16:45:35 +0100

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from '@/ui/components/seo';
+import Head from 'next/head';
+export default function Home() {
+	return (
+    <>
+    <Seo title="Coders Monkey" description="description..."/>
+    <h1>Hello World</h1>
+    </>
+  );
+}

697c077a3b2210c58ca80814447dec312a83e699
Louis7363 <louiscrt@sfr.fr>
2024-12-30 20:54:08 +0100

    debut typographie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { Seo } from '@/ui/components/seo';
+import { Typography } from '@/ui/design-system/typography/typography';
 export default function Home() {
 	return (
     <>
     <Seo title="Coders Monkey" description="description..."/>
-    <h1>Coders Monkeys web app</h1>
+    <Typography variant="h1" component="h1">
+      Welcome to Coders Monkey
+    </Typography>
     </>
   );
 }

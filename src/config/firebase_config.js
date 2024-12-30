19602d8420bbbf00ccf10631dfda3bc3cc976fc0
Louis7363 <louiscrt@sfr.fr>
2024-12-30 18:06:46 +0100

    ajout de la connection et de l'hosting avec firebase

---
+++ src/config/firebase_config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAzcJa8SnZ0WcnDVZiT7r4cIMrkprt4KSA",
+  authDomain: "codersmonkey-e11ed.firebaseapp.com",
+  projectId: "codersmonkey-e11ed",
+  storageBucket: "codersmonkey-e11ed.firebasestorage.app",
+  messagingSenderId: "446814738352",
+  appId: "1:446814738352:web:8c1a01ce59941ec3e6e875"
+};
+initializeApp(firebaseConfig); // Supprimez la variable 'app' si elle n'est pas utilisée
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Seo } from '@/ui/components/seo';
-import Head from 'next/head';
 export default function Home() {
 	return (
     <>
     <Seo title="Coders Monkey" description="description..."/>
+    <h1>Coders Monkeys web app</h1>
     </>
   );
 }
